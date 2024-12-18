807999afda9628bb76d7b2e1c15fa1d8dcd64060
sbx_user1051 <sbx_user1051@169.254.58.178>
2024-12-18 23:52:44 +0000

    Wed Dec 18 23:52:44 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,7 +6,34 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string? skus)
         {
-            return -1;
+            int AC = 0 , BC = 0, CC = 0, DC = 0;
+            if(string.IsNullOrEmpty(skus)){
+                return -1;
+            }
+            foreach(char c in skus){
+                switch (c){
+                    case 'A':
+                        AC ++;
+                        break;
+                    case 'B':
+                        BC++;
+                        break;
+                    case 'C':
+                        CC++;
+                        break;
+                    case 'D':
+                        DC++;
+                        break;
+                    default:
+                        return -1;
+                }
+            }
+            int ACDivision = AC / 3;
+            int ACMod = AC % 3;
+            int BCDivision = BC / 2;
+            int BCMod = BC % 2;
+            return (ACDivision * 130) + (ACMod * 50) + (BCDivision * 45) + (BCMod * 30) + (CC * 20) + (DC * 15);
         }
     }
 }
+
