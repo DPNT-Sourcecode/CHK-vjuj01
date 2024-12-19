f95da6a195ac1463301f2d4d9a9097b0fae05035
sbx_user1051 <sbx_user1051@169.254.58.178>
2024-12-19 00:39:08 +0000

    Thu Dec 19 00:39:08 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -31,8 +31,38 @@ namespace BeFaster.App.Solutions.CHK
             int BCMod = dict['B'] % 2;
             int FCDivision = (dict['F'] / 3) * 2;
             int FCMod = dict['F'] % 3;
+            int HCDivision1 = dict['H'] / 10;
+            int HCMod1 = dict['H'] % 10;
+            int HCDivision2 = HCMod1 / 5;
+            int HCMod2 = HCMod1 % 5;
+            int KCDivision = dict['K'] / 2;
+            int KCMod = dict['K'] % 2;
+            int NCDivision = dict['N'] / 3;
+            if (dict['M'] > NCDivision) {dict['M'] -= ECDivision;}
+            else dict['M'] = 0;
+            int PCDivision = dict['P'] / 5;
+            int PCMod = dict['P'] % 5;
+            int RCDivision = dict['R'] / 3;
+            if (dict['Q'] > RCDivision) {dict['Q'] -= RCDivision;}
+            else dict['Q'] = 0;
+            int QCDivision = dict['Q'] / 3;
+            int QCMod = dict['Q'] % 3;
+            int UCDivision = (dict['U'] / 4) * 3;
+            int UCMod = dict['U'] % 4;
+            int VCDivision1 = dict['V'] / 3;
+            int VCMod1 = dict['V'] % 3;
+            int VCDivision2 = VCMod1 / 2;
+            int VCMod2 = VCMod1 % 2;
+
             return (ACDivision1 * 200) + (ACDivision2 * 130) + (ACMod2 * 50) + (BCDivision * 45) +
-            (BCMod * 30) + (dict['C'] * 20) + (dict['D'] * 15) + (dict['E'] * 40) + ((FCDivision + FCMod) * 10);
+            (BCMod * 30) + (dict['C'] * 20) + (dict['D'] * 15) + (dict['E'] * 40) +
+            ((FCDivision + FCMod) * 10) + (dict['G'] * 20) + (HCDivision1 * 80) + (HCDivision2 * 45) + (HCMod2 * 10) +
+            (dict['I'] * 35) + (dict['J'] * 60) + (KCDivision * 150) + (KCMod * 80) + (dict['L'] * 90) +
+            (dict['M'] * 15) + (dict['N'] * 40) + (dict['O'] * 10) + (PCDivision * 200) + (PCMod * 50) +
+            (QCDivision * 80) + (QCMod * 30) + (dict['R'] * 50) + (dict['S'] * 30) + (dict['T'] * 20) +
+            ((UCDivision + UCMod) * 40) + (VCDivision1 * 130) + (VCDivision2 * 90) + (VCMod2 * 50) +
+            (dict['W'] * 20) + (dict['X'] * 90) + (dict['Y'] * 10) + (dict['Z'] * 50);
         }
     }
 }
+
