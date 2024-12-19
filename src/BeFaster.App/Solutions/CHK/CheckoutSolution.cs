061a2332685b8c8cd6de89684621fd2d08aa9aa2
sbx_user1051 <sbx_user1051@169.254.58.178>
2024-12-19 00:19:27 +0000

    Thu Dec 19 00:19:27 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,48 +6,35 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string? skus)
         {
-            int AC = 0 , BC = 0, CC = 0, DC = 0, EC = 0, FC = 0;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
             if(string.IsNullOrEmpty(skus)){
                 return 0;
             }
             foreach(char c in skus){
-                switch (c){
-                    case 'A':
-                        AC ++;
-                        break;
-                    case 'B':
-                        BC++;
-                        break;
-                    case 'C':
-                        CC++;
-                        break;
-                    case 'D':
-                        DC++;
-                        break;
-                    case 'E':
-                        EC++;
-                        break;
-                    case 'F':
-                        FC++;
-                        break;
-                    default:
-                        return -1;
-                }
+                if (char.IsUpper(c)){
+                    if (dict.ContainsKey(c)){
+                        dict[c]++;
+                    }
+                    else{
+                        dict[c] = 1;
+                    }
+                }                
             }
-            int ACDivision1 = AC / 5;
-            int ACMod1 = AC % 5;
+            int ACDivision1 = dict['A'] / 5;
+            int ACMod1 = dict['A'] % 5;
             int ACDivision2 = ACMod1 / 3;
             int ACMod2 = ACMod1 % 3;            
-            int ECDivision = EC / 2;
-            if (BC > ECDivision) {BC -= ECDivision;}
-            else BC = 0;
-            int BCDivision = BC / 2;
-            int BCMod = BC % 2;
-            int FCDivision = (FC / 3) * 2;
-            int FCMod = FC % 3;
+            int ECDivision = dict['E'] / 2;
+            if (dict['B'] > ECDivision) {dict['B'] -= ECDivision;}
+            else dict['B'] = 0;
+            int BCDivision = dict['B'] / 2;
+            int BCMod = dict['B'] % 2;
+            int FCDivision = (dict['F'] / 3) * 2;
+            int FCMod = dict['F'] % 3;
             return (ACDivision1 * 200) + (ACDivision2 * 130) + (ACMod2 * 50) + (BCDivision * 45) +
-            (BCMod * 30) + (CC * 20) + (DC * 15) + (EC * 40) + ((FCDivision + FCMod) * 10);
+            (BCMod * 30) + (dict['C'] * 20) + (dict['D'] * 15) + (dict['E'] * 40) + ((FCDivision + FCMod) * 10);
         }
     }
 }
+
 
